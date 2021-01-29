59ba130d01200b7dea0520db8d5d99a68b39c788
corinayeung <76967103+corinayeung@users.noreply.github.com>
2021-01-29 15:14:26 +0000

    removed js

--- scroll.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  //checking the page is ready and loaded.
-  $("nav a").bind("click",function(event){ event.preventDefault(); var target = $(this).attr("href"); $("html, body").stop().animate({ scrollLeft: $(target).offset().left, scrollTop: $(target).offset().top }, 950); }); 
